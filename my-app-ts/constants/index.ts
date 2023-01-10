bdbb1b1ec68ad4ca3d6badf92cecdf468572562a
Arian Hojat <armyofda12mnkeys@gmail.com>
2023-01-10 00:51:38 -0500

    added new contract

--- my-app-ts/constants/index.ts
+++ my-app-ts/constants/index.ts
@@ -1,4 +1,4 @@
-export const NFT_CONTRACT_ADDRESS = "0x9feaeb09b1eae559025D90190E55cA5ff1bC488D";
+export const NFT_CONTRACT_ADDRESS = "0x1F3833391FFD8Ea34b298CAbdAEAD2d68850188b"; //old one: "0x9feaeb09b1eae559025D90190E55cA5ff1bC488D";
 export const abi = [
     {
       "inputs": [
